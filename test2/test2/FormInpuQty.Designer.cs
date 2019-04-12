e922a2d242a9b3066d4ee5f48d77f42cfbaa3b82
Sathit Buathong <sathit_totor2@hotmail.com>
2019-04-12 14:54:32 +0700

    resize form inputQty

--- test2/test2/FormInpuQty.Designer.cs
+++ test2/test2/FormInpuQty.Designer.cs
@@ -238,7 +238,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Lavender;
-            this.ClientSize = new System.Drawing.Size(863, 435);
+            this.ClientSize = new System.Drawing.Size(366, 435);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.button10);
             this.Controls.Add(this.button11);
